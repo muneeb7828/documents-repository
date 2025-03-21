85ef3bfb8e6a5e65464f6dd7f7c1211be7dfeaeb
Muneeb ur rehman <urrehmanm51@gmail.com>
2025-03-21 17:01:30 +0530

    changes done

--- react.js
+++ react.js
@@ -168,14 +168,13 @@
 
 
 
-
-
-// css modules
+// css modules:-
 // jab bhi apan css file import karte he to vo global hoti he to isiliye css modules ka istemaal karte he taki har component ke liye ek diffrent css apply ho
 // aur jab bhi css module ki file banaenge to index.module.css ese bayenge 
 // aur jab isko import karenge to isko name dena padega aur jo name delge vo object ban jayga unsaari classes ka
 // to jab bhi hame css use karni hogi to name.classname karke karni hogi
 // aur agar class ka name hyphen ke saath he to ese use karenge name["nav-container"] 
+
 
 
 
@@ -189,7 +188,23 @@
 
 
 // aur jis bhi component ko redirect karna he us page pe to us component ke andar Link ko import karna padta he ese import { Link } from 'react-router-dom'
-// aur link tag ko lkhte he us tag pe jispe click karke page pe redirect karna he
+// aur link tag ko likhte he us tag pe jispe click karke page pe redirect karna he
+
+
+
+// useparams()
+// useparams() ye router ka hook hota he aur ye object return karta he aur iska use isliye karta he taki route ke path ko dynamic banane ke liye 
+// aur ye tabhi kaam aayga jab route me path /: ese likhenge aur iske baad jo bhi likhenge vo property ban jaygi aur browser ke location me jo / iske baad jo likhenge vo iski value ban ban jaygi
+
+
+
+
+// parent child route
+// agar ham route ke andar route banate he to usi ko parent child route bolte he
+
+
+// outlet
+// aur isko parent component me import karte he taki jo child componenet he uski position ke liye ke kaha par rakhna he
 
 
 
@@ -199,6 +214,20 @@
 // Controlled component vo hote jo react controlled kar raha he jese usesate se ya eventhandler se
 
 // aur uncontrolled component vo hote he jo react handle nahi kar raha jese dom se value get karna ya useref ka use karke value get karna etc
+
+
+
+
+// axious
+// axious ye bhi fetch ki tarah hota he bas isse asaan hojata he data ko lena 
+
+
+
+
+
+
+
+
 
 
 
