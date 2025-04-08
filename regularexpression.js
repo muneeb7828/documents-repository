36fc7cc4f29d00a8fd77f184e1245ab6474eaa7c
Muneeb ur rehman <urrehmanm51@gmail.com>
2025-04-08 13:15:51 +0530

    changes done

--- regularexpression.js
+++ regularexpression.js
@@ -3,7 +3,7 @@
 
 // regular expressions:- 
 // ye pattern banene ke kaam ati he aur regexr.com is web pe check kar sakte he saari chezien
-// ye basically bpassword email ko banane me kaam ata he 
+// ye basically password email ko banane me kaam ata he 
 // aur ye he iska syntax /very/gimus 
 
 
@@ -27,20 +27,20 @@
 // \w= ye word ke liye hota he
 // \d= ye digit ke liye hota he
 // \s= ye whitespace ke liye hota he
-// \W= Not word
+// \W= Not word @#$%
 // \D= Not dogit
 // \S= Not whitespace
 // [abc]= isse iske andar jitne bhi word hote he un sabko select kar leta he
 // [^abc]= ye nahi select karne ke liye hota he 
-// [a-g]= isse a to g sab select ho jate he
+// [a-g]= isse a to g koi sa bhi word select ho jayga
 
 
 // Anchors characters
 // ^abc$ \b \B
 
 // ^abc$=agar word a se shuru hoga aur c pe khatam hoga tabhi chalega
-// \bmuneeb=isse agar vo word he to ajayga 
-// \Bmuneeb=isse agar he to nahi ayega
+// muneeb\b=ye batatahe he ki word yaha pe khatam ho raha he
+// muneeb\B=ye tab chalta he jab word ki koi boundry nahi ho
 
 
 // Escaped characters
@@ -57,13 +57,16 @@
 // (abc) \1 (?:abc) (?=abc) (?!abc)
 
 // (abc)=isse word ki grouping ho jati he agar word isse milega tabhi select hoga nahi to nahi hoga
+// (?:abc)=
+// (?=a)=ye tabhi chalega jab aage a likha hoga
+// (?!a)=ye tabhi chalega jag aage a nahi likha hoga
 
 
 // Quantifiers charachters
 // a* a+ a?  a{5} a{2,} a{1,3} a+? a{2,}? ab|cd
 
 
-// a*= isse word 0 ya usse zada select ho jayga
+// a*= isse word ho ya na ho tobhi select ho jayga
 // a+= isse word 1 ya usse zada select ho jayga
 // b{5}= isse same word 5 hoga tabhi chalega 
 // b{5,}= isse same word 5  ya usse zada hoga tabhi chalega 
@@ -72,4 +75,12 @@
 
 
 let regex= /[^][a-z][a-z]eeb/g
-let regex2=/^[a-zA-z1-9@#$%^0]{8,}@gmail.com$/g
+let regex2=/^[a-zA-z1-9@#$%^0]{8,}@gmail.com$/.test(variable)
+
+// .test() ye bolean return karta he
+// .source() ye regular expression ke andar ka content batata he
+// .exec() ye check karne ke kaam ata he agar word string me he to usko array me return karta he agara nahi he to null return karta he
+//  aur agar word ek se zada he to phele wala ka hi index return karega agar g flag lagalenge to sab ka index return karega 
+// variable.match(reg) ye array aur null return karta he
+// variable.search(reg) ye index return karata agar vo he to nahi to -1 return karta he
+
