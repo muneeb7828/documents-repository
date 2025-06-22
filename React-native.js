826b21734ab40d29e31074fb772b20517f93f77e
muneeb3778 <murrehman377@gmail.com>
2025-06-22 15:28:14 +0530

    change sodne

--- React-native.js
+++ React-native.js
@@ -2,12 +2,12 @@
 // ye basically mobile apps banane ke kaam ati he
 
 
+// difference between package.json and packagelock.json
+// is dono me ye difference hota package.json isme jo bhi project banane me jo library install karte he vo isme aati he 
+// aur packagelock.json isme jo library he unki bhi dependency library ati he
 
-
-
-
-
-
+// ye command likhne se kya kya issues arahe he connect karne me ye pata chal jata he
+// npx react-native doctor
 
 
 
