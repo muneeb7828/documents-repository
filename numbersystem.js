48be2731667419b6286b323b9545cd0c5c42f412
muneeb3778 <murrehman377@gmail.com>
2025-06-24 22:54:58 +0530

    changes done

---
+++ numbersystem.js
@@ -0,0 +1,60 @@
+
+// number system 
+// number system ki madad se numbers ko represent karte he
+// aur har number system ka positional weight hota he
+
+
+// number system types
+
+// decimal number system
+// decimal number yani jo point me hote he for exp 10.10
+// aur iska base 10 hota he kyuki isme 10 symbol hote he 0,1,2,3,4,5,6,7,8,9
+// iska positional weight hota he 10 to the power 0 and then 10 to the power 1 and more
+
+// binary number system
+// binary numbers se pura computer bana he aur isme bas 2 symbol hote he 0,1 isliye iska base 2 hota he
+// iska positional weight hota he 2 to the power 0 and then 2 to the power 1 and more
+
+// octal number system
+// octal used in computer science as a shorthand way to represent binary (base-2) numbers, where every three binary digits can be represented by a single octal digit
+// octal number me 8 symbols hote he 0 to 7 to iska base hota he 8
+// iska positional weight hota he 8 to the power 0 and then 8 to the power 1 and more
+
+// convert octal number into binary
+// 0 octal number = 000 binary number
+// 1 octal number = 001 binary number
+// 2 octal number = 010 binary number
+// 3 octal number = 011 binary number
+// 4 octal number = 100 binary number
+// 5 octal number = 101 binary number
+// 6 octal number = 110 binary number
+// 7 octal number = 111 binary number
+
+// 546 octal number = 101100110 binary number
+// to is tarike se kisi bhi octal number ko binary me convert kar sakte he
+
+
+// hexadecimal number system
+// hexadecimal number me 16 symbols hote he 0 to 9 and a to f,  a stands for 10, b stands for 11, c stands for 12 etc
+// iska positional weight hota he 16 to the power 0 and then 16 to the power 1 and more
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- type-script.ts
+++ type-script.ts
@@ -43,6 +43,7 @@
 // ise andar data types define kar sakte he aur isko static typing bhi bolte he aur js me nahi kar sakte
 // aur isse ye benifit hota he agar kisi variable ka data type string define kar diya to agar us variable ko change karenge to string hi dena padega
 // aur isme multilpe data type bhi de sakte he
+var data : null | string = null       // is tarike se
 // aur is tarike se static typing data type define karte he
 var age:number=24
 
@@ -69,14 +70,56 @@ var age:number=24
 
 // primitive types
 // number,string,boolean,null,undefined,bigint,symbol
-// bigint tab use karte he jab number bohot bata hota he
+// bigint tab use karte he jab number bohot bata hota he  
+   var bignumber=92390239232323400n         // ye number he isme agar koisa number add kar rahe he to add nahi ho raha taki ek limit ke baad ye add nahi hota to iske liye bigint k a use karenge last me n lagake   
+   var x=1n;
+   console.log(bignumber+x)
+   
 // symbol ye unique address generate karta he ye he iska syntax Symbol('description')
+var sym=Symbol('abc');
+var sym2=Symbol('abc');
+console.log(sym==sym2)     // ye false return karega
+const id=Symbol('id')
+const obj={
+   [id]:100,                // is tarike se key likhte he to variable ki value iski key ban jati he
+   name:"muneeb"
+}
+console.log(obj[id])        // aur isko access is tarike se karte he   
+
+// number me ye values bhi hoti he decimal,octal,binary,hexadecimal
+var oct:number=0o1000001
+var hexa:number=0x00001
+var binary:number=0b00001
+//convert string to numeber
+var stirng="100"
+var num=Number(stirng)
+
+// type inference wih number
+// inference matlab andaza lagana agar variable ke sath datatype define nahi kiya to bhi ye pata laga leta he data type isi ko type inference bolte he
+
 
 // Object types
 // Special types
 // Advanced types 
 // Function types
 
+
+
+
+// tsc configuration file
+
+// how to generate config file
+// tsc --init   is command se generate hote he config file
+
+// use of coding file
+
+// "target": "es2024",   
+// tsconfig file me ek property hoti he target name se ye is liye hoti he ke aap konsa version use kar rahe echma script ka ts ko js me convert karne ke liye
+
+// convert all ts file to js together
+// tsc     is command se ek baar me hi saari files js me convert hojati he
+
+// fix errors after config
 
 
 
