d4951d614b84cebc513e56aab3fba0b1e663184e
muneeb3778 <murrehman377@gmail.com>
2025-07-24 22:20:27 +0530

    changes

--- React-native.js
+++ React-native.js
@@ -34,7 +34,7 @@
 
 // aur npx react-native run-android agar ye command chalane ke baad kuch error aay to ye command chalayenge
 // adb kill-server, adb start-server
-
+// npx react-native start --reset-cache
 
 
 
@@ -120,9 +120,13 @@
 
 // View component
 // view component jo hota he vo parent div ki tarah hota he aur jab jsx likhte he to view se hi wrap karte he aur isko khali button pe bhi wrap kar sakte he
+// View doesn't support onPress in React Native.
+// To make a non-button component respond to taps, you must wrap it in a TouchableOpacity, TouchableWithoutFeedback, or Pressable.
+
 
 // Text component 
 // ye isliye hota he jese khuch text likhna ho to iska use karte he
+// aur isme onPress event use kar sakte he
 
 // Button component
 // isme button ko ese likhte he
@@ -305,7 +309,7 @@ backgroundColor:"black",
   //  }
 
 // and this is how we change state in class component
-// aur isme fun`tion ka kuch bhi rakh sakte he aur isme jo setState he ye hi state ki value ko change karta he
+// aur isme function ka kuch bhi rakh sakte he aur isme jo setState he ye hi state ki value ko change karta he
 
 // updatename(){
 //  this.setState({name:"Muneeb ur rehman"})  
@@ -333,32 +337,181 @@ backgroundColor:"black",
 
 // show hide toggle component
 
+// const ToggeComponet = () => {
+//  const [show,setshow]=useState(true)
+//     return (
+//     <View>
+//       <Text style={{fontSize:30}}>Show/hide Component</Text>
+//       <Button title='toggle Component' onPress={()=>{setshow(!show)}}/>
+//       {
+//        show==true?<User/>:null
+//       }
+//     </View>
+//   )
+// }
+
+
+// const User=()=>{
+//  return <View>
+//    <Text style={{fontSize:30,color:'green'}}>User Component</Text> 
+//   </View>  
+// }
 
 
 
+// TouchableOpacity
+// ye Buutton component jese hi hoti bas isme styling bhi kar sakte he button me nahi kar sakte aur isme single closing tag dono hote he
+
+
+// Activity Indicator (loader)
+// is component ka use karke loader dikha sakte he aur ye single tag hota he aur size property ke size adjust kar sakte he aur color property se color change kar sakte he
+
+// Modal in react native
+// ye pop up ke liye hota he aur bedefault puri height cover kar leta he to jo text likha hota he to hide hojata he to hide se hatane ke liye transparent={true} karna padta he
+
+// <Modal transparent={true}>
+//  <View>
+//   <View>
+//      <Text>Hello Code step by step<Text/>
+//   <View/>
+//  <View/>
+// <Modal/>
+
+// pressable Event
+// aur ye tab use karte he jab button pe multiple events lagana ho
+// isme 4 event hote he onpress , onpressin , onpressout ,onpresslong
+
+
+// Apply NativeWind in react native
+
+// To convert this React Native AppContext file to use NativeWind, you will:
+//  Replace StyleSheet.create styles with Tailwind utility classes
+//  Use className prop instead of style
+
+
+// Install and configure NativeWind
+
+// 1 Install NativeWind:
+
+// npm install nativewind
+// npm install metro-react-native-babel-preset
+
+// 2 Update babel.config.js:
+
+// module.exports = {
+//   presets: ['module:metro-react-native-babel-preset'],
+//   plugins: ['nativewind/babel'],
+// };
+
+
+// 3 Create tailwind.config.js:
+
+// npx tailwindcss init
+
+
+// tailwind.config.js
+
+// module.exports = {
+//   content: [
+//     "./App.{js,jsx,ts,tsx}",
+//     "./AllComponents/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+
+// Updated AppContext.js Using NativeWind:
+
+// import React, { createContext, useState } from "react";
+// import { View, Text, Image, TextInput, TouchableOpacity, CheckBox } from "react-native";
+// import { useNavigation } from "@react-navigation/native";
+
+// // Images (make sure these exist in assets folder and paths are correct)
+// const LogoutImage = require("../assets/Walletimages/Logoutimage.png");
+// const VectorIcon = require("../assets/Walletimages/Vector.png");
+// const FaceIcon = require("../assets/Walletimages/Group77.png");
+// const PasscodeIcon = require("../assets/Walletimages/Group78.png");
+// const UAEPassLogo = require("../assets/Loginimages/Group40.png");
+// const LineDivider = require("../assets/Loginimages/blackline.jpg");
+
+// const AppContext = createContext();
+
+// const AppProvider = (props) => {
+//   const [islogin, setislogin] = useState(false);
+//   const [isquicklogin, setisquicklogin] = useState(false);
+//   const [changefooterclr, setchangefooterclr] = useState(1);
+//   const navigation = useNavigation();
+
+//   const Cards = () => (
+//     <View>
+//       {!isquicklogin && (
+//         <TouchableOpacity
+//           className="h-11 bg-yellow-700 justify-center items-center rounded-md mx-[8%] mt-[80%]"
+//           onPress={() => navigation.navigate("Login")}
+//         >
+//           <Text className="text-white font-bold text-xs">Log in</Text>
+//         </TouchableOpacity>
+//       )}
+
+//       {!isquicklogin && (
+//         <TouchableOpacity onPress={() => setisquicklogin(true)}>
+//           <Image source={LogoutImage} className="h-11 w-[85%] mx-[8%] mt-2" resizeMode="contain" />
+//         </TouchableOpacity>
+//       )}
+//     </View>
+//   );
+
+// 
+
+
+// and for type script we have to add this file 
+// nativewind-env.d.ts and my-app.d.ts
+// and add this line into bhot this file
+// <reference types="nativewind/types"/>
 
 
 
+// this is for phone connect
+// adb kill-server
+// adb start-server
+// adb devices
 
+// Status Bar in React native
+// ye jo phone me top pe icons dikhte he usi ko status bar bolte he
+// iska use karke styling change kar sakte he status bar ki
+// sabse phele isko react native se import karna padta he fir isko View ke andar likhna hota he
 
+// <View>
+// <StatusBar
+// backgroundColor="orange"
+// barStyle="dark-content"   isse icons ka color black ho jata he
+// hidden={true}             isse pura status bar hide ho jata he
+//  />
+// </View> 
 
+// platform in React Native
+// iska matlab aap phone pe konsa operating system use kar rahe ho android ya ios ya windows
+// iska use isliye karte he taki android pe kuch aur dikhana he aur ios pe kuch dikhana he
+// aur ye bhi react native se import hoga
+// aur isko ese use karte he Platform.OS to ye platform ka name batata he
 
+// npm i react-native-webview 
+// ye karne se koisi bhi web phone pe dekh sakte he
 
+// Navigation in React-Native
+// ye browerserrouter ki tarah hoti he 
+// aur isme 3 tarike ki navigtion hoti he stack, drawer, tab
 
+// stack navigation
+// jab bhi ek page se dusre page pe jana ho to stack navigation ka use karenge
 
+// drawer navigation
+// jab bhi kuch slide karte he jese menu pe click parte he to ek slider ajat ahe usi ko drawer navigation bolte he
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// tab navigation
+// ye kya karta he ki jab button pe click karne par ya slide karne par dusre component pe jay to usi ko tab navigation bolte he
 
 
