78a0ceff3cf08c31daaa031931a34382485fc602
muneeb3778 <murrehman377@gmail.com>
2026-01-10 17:39:56 +0530

    changes done in React-Native-JestTesting.js

--- React-Native-JestTesting.js
+++ React-Native-JestTesting.js
@@ -2,3 +2,6 @@
 // How jest works in react native app
 // https://chatgpt.com/share/6960cb0e-4d4c-800c-abee-f1a5a520324d
 
+
+// This is the flow of jest testing
+// https://chatgpt.com/share/69623acd-fd14-800c-841f-bdad3219d855
